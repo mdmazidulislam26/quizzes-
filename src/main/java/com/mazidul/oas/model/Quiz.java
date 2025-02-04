993e029ad42772a7c74ab3e672bb11afc05a2cce
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2025-02-04 10:31:51 +0600

    Add files via upload

---
+++ src/main/java/com/mazidul/oas/model/Quiz.java
@@ -0,0 +1,52 @@
+package com.mazidul.oas.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    @ManyToMany
+    private List<Question> questions;
+
+    public Quiz() {
+    }
+
+    public Quiz(Integer id, String title, List<Question> questions) {
+        this.id = id;
+        this.title = title;
+        this.questions = questions;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
